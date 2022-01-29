4fc4b742fe37f515eddde47d4114214867039e6a
Oskari Mäki <oskari.maki@hotmail.com>
2022-01-30 00:20:54 +0200

    colors!! woowow !

--- GGJ2022/Assets/Scripts/Plate.cs
+++ GGJ2022/Assets/Scripts/Plate.cs
@@ -14,20 +14,47 @@ public class Plate : MonoBehaviour
         Blue
     }
 
-    private Renderer rend;
-
+    public GameObject baseMaterialObject;
+    private Material bm0;
     private void Start()
     {
-        rend = GetComponent<Renderer>();
+        int rand = Random.Range(1, 20);
+        int rand2 = Random.Range(1, 20);
+        if (rand > 17)
+        {
+            plateType = PlateType.Red;
+        } else
+        {
+            if (rand2 > 17)
+            {
+                plateType = PlateType.Blue;
+            }
+        }
+
+        if (baseMaterialObject != null)
+        {
+            if (baseMaterialObject.GetComponent<Renderer>().material != null)
+            {
+                bm0 = new Material(baseMaterialObject.GetComponent<Renderer>().material);
+                baseMaterialObject.GetComponent<Renderer>().material = bm0;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        PulseColor(Color.red);
+        if(plateType == PlateType.Red)
+        {
+            PulseColor(Color.red);
+        }
+        if (plateType == PlateType.Blue)
+        {
+            PulseColor(Color.blue);
+        }
     }
 
     public void PulseColor(Color color)
     {
-        rend.material.color = new Color(color.r + Mathf.Sin(Time.time) * 10, color.g, color.b);
+        bm0.color = new Color(color.r + Mathf.Max(0.6f, Mathf.Sin(Time.time * 4) * 2), color.g + Mathf.Max(0.6f, Mathf.Sin(Time.time * 4) * 2), color.b + Mathf.Max(0.6f, Mathf.Sin(Time.time * 4) * 2));
     }
 }
