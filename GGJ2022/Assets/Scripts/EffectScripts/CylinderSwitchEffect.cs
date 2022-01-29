e46d9fc3a24c0c6e074e46092cba105ae7219f62
Oskari Mäki <oskari.maki@hotmail.com>
2022-01-29 04:04:23 +0200

    yea

--- GGJ2022/Assets/Scripts/EffectScripts/CylinderSwitchEffect.cs
+++ GGJ2022/Assets/Scripts/EffectScripts/CylinderSwitchEffect.cs
@@ -10,20 +10,21 @@ public class CylinderSwitchEffect : MonoBehaviour
     private List<Material> runtimeMaterialList = new List<Material>();
 
     public GameObject cylinder;
+    public GameObject cylinder2;
 
-    private Material currMaterial;
+
 
     private int idx;
 
     public float switchSpeed;
 
-    private bool isSwitching;
 
     public float timeBetweenSwitches;
 
+    private Color previousColor;
+
     void Start()
     {
-        currMaterial = cylinder.GetComponent<Renderer>().material;
         Color color = new Color(1, 1, 1, 1);
         foreach (Material m in materialList)
         {
@@ -32,7 +33,9 @@ public class CylinderSwitchEffect : MonoBehaviour
             m0.SetColor("_BaseColor", color);
         }
         cylinder.GetComponent<Renderer>().material = materialList[0];
+        cylinder2.GetComponent<Renderer>().material = materialList[1];
         materialList[idx].SetColor("_BaseColor", new Color(materialList[idx].color.r, materialList[idx].color.g, materialList[idx].color.b, 1));
+        materialList[idx + 1].SetColor("_BaseColor", new Color(materialList[idx].color.r, materialList[idx].color.g, materialList[idx].color.b, 0));
         StartCoroutine(switchCylinder());
     }
 
@@ -40,36 +43,34 @@ public class CylinderSwitchEffect : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float offset = Time.time * 0.5f;
+        float offset = Time.time * 0.1f;
         runtimeMaterialList[idx].mainTextureOffset = new Vector2(offset, offset);
+        //runtimeMaterialList[idx].SetColor("_EmissionColor", Color.Lerp(GetRandomColor(), GetRandomColor(), 1f));
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
     }
 
     IEnumerator switchCylinder()
     {
-        isSwitching = true;
         //materialList[idx].SetColor("_BaseColor", new Color(materialList[idx].color.r, materialList[idx].color.g, materialList[idx].color.b, 1));
         for (float i = 0; i < 1; i += 0.01f)
         {
             Color color = new Color(1, 1, 1, 1 - i);
+            Color color2 = new Color(1, 1, 1, 1 + i);
             runtimeMaterialList[idx].SetColor("_BaseColor", color);
+            if(idx + 1 == materialList.Count - 1)
+            {
+                runtimeMaterialList[0].SetColor("_BaseColor", color2);
+            } 
+            else
+            {
+                runtimeMaterialList[idx + 1].SetColor("_BaseColor", color2);
+            }
             yield return new WaitForSeconds(switchSpeed);
         }
-        if (idx == materialList.Count - 1)
-        {
-            idx = 0;
-        } else
-        {
-            idx++;
-        }
-        cylinder.GetComponent<Renderer>().material = runtimeMaterialList[idx];
-        currMaterial = runtimeMaterialList[idx];
-        for (float i = 0; i < 1; i += 0.01f)
-        {
-            Color color = new Color(1, 1, 1, 0 + i);
-            runtimeMaterialList[idx].SetColor("_BaseColor", color);
-            yield return new WaitForSeconds(switchSpeed);
-        }
-        isSwitching = false;
         yield return new WaitForSeconds(timeBetweenSwitches);
         StartCoroutine(switchCylinder());
     }
